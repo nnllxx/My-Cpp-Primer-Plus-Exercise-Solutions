82538d5728863df923df1551acf37dc85916d6a5
nnllxx <166314299+nnllxx@users.noreply.github.com>
2025-10-24 21:13:56 +0800

    Create ch06_ex03.cpp

---
+++ chapter6/ch06_ex03.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << R"(Please enter one of the following choices:
+c) carnivore        p) pianist
+t) tree             g) game
+)";
+	char ch;
+	cin >> ch;
+	while((ch!='c') and (ch!='p') and (ch!='t') and (ch!='g')){
+		cout << "Please enter a c, p, t or g: ";
+		cin >> ch;
+	};
+	switch(ch){
+		case 'c':
+			cout << "car-car-carnivore" << endl;
+			break;
+		case 'p':
+			cout << "pi-pi-pianist" << endl;
+			break;
+		case 't':
+			cout << "tree-ee-ee" << endl;
+			break;
+		case 'g':
+			cout << "ga-ga-game" << endl;
+			break;
+	};
+	return 0;
+}
