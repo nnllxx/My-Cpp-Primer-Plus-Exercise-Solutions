088d6413a8749fcca82f0bf783fc3f3f9a8bcb70
nnllxx <166314299+nnllxx@users.noreply.github.com>
2025-10-29 19:45:34 +0800

    Update ch04_ex02.cpp

--- chapter04/ch04_ex02.cpp
+++ chapter04/ch04_ex02.cpp
@@ -2,22 +2,16 @@
 #include <string>
 using namespace std;
 
-int main(){
-	string first_name;
-	string last_name;
-	char deserve_grade;
-	int age;
-	cout << "What is your first name? ";
-	cin >> first_name;
-	cout << "What is your last name? ";
-	cin >> last_name;
-	cout << "What letter grade do you deserve? ";
-	cin >> deserve_grade;
-	char grade = deserve_grade + 1;
-	cout << "What is your age? ";
-	cin >> age;
-	cout << "Name: " << last_name << ", " << first_name << endl
-		<< "Grade: " << grade << endl
-		<< "Age: " << age << endl;
-	return 0; 
+int main()
+{
+    string name;
+    string dessert;
+
+    cout << "Enter your name:\n";
+    getline(cin, name); 
+    cout << "Enter your favorite dessert:\n";
+    getline(cin, dessert);
+    cout << "I have some delicious "<< dessert;
+    cout << " for you, "<< name << "\n";
+    return 0;
 }
