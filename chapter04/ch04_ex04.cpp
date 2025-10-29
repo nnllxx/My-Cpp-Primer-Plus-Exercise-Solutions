189b50dfb20bd2f2bc499cdecdbeb48d724fee26
nnllxx <166314299+nnllxx@users.noreply.github.com>
2025-10-29 19:58:02 +0800

    Create ch04_ex04.cpp

---
+++ chapter04/ch04_ex04.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string first_name;
+    string last_name;
+    cout << "Enter your first name: ";
+    cin >> first_name;
+    cout << "Enter your last name: ";
+    cin >> last_name;
+    cout << "Here's the information in a single string: "
+    	<< last_name + ", " + first_name << endl;
+    return 0;
+}
