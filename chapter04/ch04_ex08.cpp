02ee8185c9ab157f7867a08885838be6d7d2b54f
nnllxx <166314299+nnllxx@users.noreply.github.com>
2025-10-29 20:27:00 +0800

    Create ch04_ex08.cpp

---
+++ chapter04/ch04_ex08.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Pizza{
+	string corp_name;
+	float diameter;
+	float weight;
+};
+
+int main()
+{	
+	Pizza * pt = new Pizza;
+	cout << "Enter the diameter: ";
+	cin >> pt->diameter;
+	cin.get();   // 清除缓冲区剩下的回车
+	cout << "Enter the corp_name: ";
+	getline(cin, pt->corp_name);
+	cout << "Enter the weight: ";
+	cin >> pt->weight;
+	cout << "corp_name: " << pt->corp_name << endl
+		<< "diameter: " << pt->diameter << endl
+		<< "weight: " << pt->weight << endl;
+	return 0;
+}
