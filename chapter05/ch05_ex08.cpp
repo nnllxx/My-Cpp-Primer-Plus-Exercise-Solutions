6c60a5e60e677f68f0368b4f80ab37a84b24d929
nnllxx <166314299+nnllxx@users.noreply.github.com>
2025-11-02 23:00:30 +0800

    Create ch05_ex08.cpp

---
+++ chapter05/ch05_ex08.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main(){
+    char word[20];
+    int count = 0;
+    count = 0;
+    cout << "Enter words (to stop, type the word done): ";
+    do{
+        cin >> word;
+        count += 1;
+    }while(strcmp(word, "done"));   // strcmp()会一直比较直到遇到任一个字符串的'\0'结束符，若相同则返回0。
+    cout << "You entered a total of " << count-1 << " words.";
+    return 0;
+}
