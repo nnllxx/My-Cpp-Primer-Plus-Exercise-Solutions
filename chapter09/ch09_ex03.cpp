65039e6ce62900b4d133efba487a56481014c4ef
nnllxx <166314299+nnllxx@users.noreply.github.com>
2025-11-05 00:04:34 +0800

    Update ch09_ex03.cpp

--- chapter09/ch09_ex03.cpp
+++ chapter09/ch09_ex03.cpp
@@ -10,7 +10,7 @@ struct chaff{
 };   // 可以验证 sizeof(chaff) 为24
 
 int main(){
-    chaff * pt = new (buffer) chaff[2];
+    chaff * pt = new (buffer) chaff[2];   //使用定位new，不需要delete
     strcpy(pt[0].dross, "aaaa-bbbb-cccc-");
     pt[0].slag = 1;
     strcpy(pt[1].dross, "1111-2222-3333-");
