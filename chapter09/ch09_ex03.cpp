11d22bb8a95ff827704cf7a8811154988fc58412
nnllxx <166314299+nnllxx@users.noreply.github.com>
2025-11-04 22:16:45 +0800

    Create ch09_ex03.cpp

---
+++ chapter09/ch09_ex03.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+const int BUF = 50;
+char buffer[BUF];
+
+struct chaff{
+    char dross[20];
+    int slag;
+};   // 可以验证 sizeof(chaff) 为24
+
+int main(){
+    chaff * pt = new (buffer) chaff[2];
+    strcpy(pt[0].dross, "aaaa-bbbb-cccc-");
+    pt[0].slag = 1;
+    strcpy(pt[1].dross, "1111-2222-3333-");
+    pt[1].slag = 2;
+    for(int i=0;i<2;i++){
+        cout << "#" << i+1 << ": \n";
+        cout << "dross: " << pt[i].dross << endl;
+        cout << "slag: " << pt[i].slag << endl;
+    }
+    return 0;
+}
