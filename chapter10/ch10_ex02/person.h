a581c473395c57d36e6554e0869b7111b68fc3a7
nnllxx <166314299+nnllxx@users.noreply.github.com>
2025-11-16 12:19:38 +0800

    Create person.h

---
+++ chapter10/ch10_ex02/person.h
@@ -0,0 +1,14 @@
+#include <string>
+using namespace std;
+
+class Person {
+private:
+    static const int LIMIT = 25;
+    string lname;    // Person's last name
+    char fname[LIMIT];  // Person's first name
+public:
+    Person() {lname = ""; fname[0] = '\0'; } // #1
+    Person(const string & ln, const char * fn = "Heyyou");  // #2
+    void Show() const;    // firstname lastname format
+    void FormalShow() const;  // lastname, firstname format
+};
